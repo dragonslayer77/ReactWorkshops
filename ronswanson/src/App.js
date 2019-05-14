4ee70c079c13f281b059025b1b56b6f43f3a250e
Isabella <isabellaafry@gmail.com>
2019-05-14 17:48:25 +0100

    ron swanson

--- ronswanson/src/App.js
+++ ronswanson/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <img src="https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif" alt="ron" />
-      <p>QUOTE</p>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      quote:''
+    }
+  }
+  loadData(){
+    fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+      .then(response => response.json())
+      .then((data) => {
+          this.setState({
+              quote: data
+          });
+      });
+  }
+
+  setTimer = () => {
+    this.timer = setInterval(()=>this.loadData(), 30000);
+}
+
+  componentDidMount() {
+    this.setTimer();
+    this.loadData();
+   
+  }
+
+  componentWillUnmount = () => {
+    this.timer=null;
+
+}
+
+  render(){
+    return (
+      <div className="App">
+        <img src="https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif" alt="ron" />
+        <p>{ this.state.quote }</p>
+      </div>
+    );
+  }
 }
 
 export default App;
